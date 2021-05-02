1e31e0910ee627ee40dca6d8045bbfca412c7695
汪涵铭 <yukiyukimu.snow@gmail.com>
2021-05-02 15:26:58 +0800

    Testing hash table

--- lsh/test_hash_table.py
+++ lsh/test_hash_table.py
@@ -8,17 +8,19 @@ import cv2
 A module is a file containing Python code. 
 A package, however, is like a directory that holds sub-packages and modules.
 '''
-path = r'../data/set1/set1-1.jpg'
-img = image2vector.read_image(path)
-img_resized = cv2.resize(img, dsize=(500, 500))
-vector = image2vector.image_to_vector(img_resized)
-d = len(vector)
-vector = vector.T
+path1 = r'../data/set1/set1-1.jpg'
+img1 = image2vector.read_image(path1)
+img1_resized = cv2.resize(img1, dsize=(500, 500))
+vector1 = image2vector.image_to_vector(img1_resized)
+d = len(vector1)
+vector1 = vector1.T
 # print(vector.shape)
 table = ht.HashTable(5, d)
-value = table.generate_hash(vector)
+# value = table.generate_hash(vector)
 # print(value)
-table.__setitem__(vector, "label1")
+table.__setitem__(vector1, "image1")
+vector2 = vector1
+table.__setitem__(vector2, "image2")
 
 items = table.hash_table.items()
 for item in items:
